be252c5b7111f13644dd217db3aaf35672c2cf3c
Ben Racine <ben.racine@axiomspace.com>
2025-08-25 06:18:54 -0500

    .

--- src/main.py
+++ src/main.py
@@ -1,23 +1,28 @@
 from rocket_sim import create_world, create_system, SIM_TIME_STEP
 import numpy as np
-# import polars as pl
+import polars as pl
 import elodin as el
 # import matplotlib.pyplot as plt
 
+
 def main():
-    # create jax array range of pitch angles from 0 to 90 degrees by 5 degree increments
-    # pitch_angles = jnp.arange(0, 91, 5)
+    # Explore pitch angle array
+    # jnp.arange(0, 91, 5)
     pitch_angles = np.array([15, 30])
+
+    # Use monte carlo approach to find optimal launch angle for max range
+
     # Run the simulation over the range of pitch angles
     for pitch in pitch_angles:
         print(f"\n--- Running simulation for pitch angle: {pitch} degrees ---")
         w = create_world(pitch)
         sys = create_system()
-        w.run(sys, sim_time_step=SIM_TIME_STEP, max_ticks=1200)
+        w.run(sys, sim_time_step=SIM_TIME_STEP, max_ticks=1200, optimize=True)
 
+        # Investigate the historical data
+        # Unable to get here for some reason I wasn't yet able to debug
         el.Exec(w).history("Rocket.world_pos")
 
-        """
         df = w.history("Rocket.world_pos")
         df = df.with_columns(
             pl.col("Rocket.world_pos").arr.get(4).alias("Rocket.x"),
@@ -25,16 +30,9 @@ def main():
             pl.col("Rocket.world_pos").arr.get(6).alias("Rocket.z"),
         )
         print(df)
-        """
 
-    """
-    distance = np.linalg.norm(df.select(["Rocket.x", "Rocket.y", "Rocket.z"]).to_numpy(), axis=1)
-    df = df.with_columns(pl.Series(distance).alias("distance"))
-    ticks = np.arange(df.shape[0])
-    fig, ax = plt.subplots()
-    ax.plot(ticks, df["distance"])
-    plt.show()
-    """
+    # After running all simulations, find the max range where z is still positive
+
 
 if __name__ == "__main__":
     print("Starting simulation...")
